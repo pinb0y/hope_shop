3e8edce3c4561b75bc28a6ed90a5462181d0ce72
Pinchuk Sergey <Pinboyer@gmail.com>
2024-09-11 22:59:52 +0300

    добавил форму версии и создал формсет

--- product_catalog/forms.py
+++ product_catalog/forms.py
@@ -1,16 +1,21 @@
-from django import forms
+from django.core.exceptions import ValidationError
+from django.forms import BooleanField, ModelForm
 
-from product_catalog.models import Product
+from product_catalog.models import Product, Version
+
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs )
+        super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs["class"] = "forms-control"
+            if isinstance(field, BooleanField):
+                field.widget.attrs["class"] = "form-check-input"
+            else:
+                field.widget.attrs["class"] = "form-control"
 
 
 
-class ProductForm(StyleFormMixin, forms.ModelForm):
+class ProductForm(StyleFormMixin, ModelForm):
     taboo_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция",
                    "радар"]
 
@@ -22,13 +27,17 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         cleaned_data = self.cleaned_data["product_name"]
         for word in self.taboo_words:
             if word in cleaned_data.lower():
-                raise forms.ValidationError(f"слово <{word}> нельзя использовать в имени")
+                raise ValidationError(f"слово <{word}> нельзя использовать в имени")
         return cleaned_data
 
     def clean_description(self):
         cleaned_data = self.cleaned_data["description"]
         for word in self.taboo_words:
             if word in cleaned_data.lower():
-                raise forms.ValidationError(f"слово <{word}> нельзя использовать в описании")
+                raise ValidationError(f"слово <{word}> нельзя использовать в описании")
         return cleaned_data
 
+class VersionForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = Version
+        fields = "__all__"
--- product_catalog/views.py
+++ product_catalog/views.py
@@ -1,6 +1,5 @@
 from django.forms import inlineformset_factory
-from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
@@ -10,7 +9,7 @@ from django.views.generic import (
 )
 from django.views.generic.base import TemplateView
 
-from product_catalog.forms import ProductForm
+from product_catalog.forms import ProductForm, VersionForm
 from product_catalog.models import Product, Version
 
 
@@ -40,24 +39,29 @@ class ProductUpdateView(UpdateView):
     form_class = ProductForm
     success_url = reverse_lazy("catalog:product_list")
 
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        if self.request.method == "POST":
+            context_data["formset"] = ProductFormset(self.request.POST, instance=self.object)
+        else:
+            context_data["formset"] = ProductFormset(instance=self.object)
+        return context_data
 
-
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data["formset"]
+        if form.is_valid and formset.is_valid():
+            self.object.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:product_list")
-
-
-# def toggle_activity(request, pk):
-#     product_item = get_object_or_404(Product, pk=pk)
-#     if product_item.is_active:
-#         product_item.is_active = False
-#     else:
-#         product_item.is_active = True
-#
-#     product_item.save()
-#
-#     return redirect(reverse("catalog:product_list"))
 
 
 class ContactTemplateView(TemplateView):
